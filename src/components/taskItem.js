4b7241f39ffb95c7d7fb8c04eff9425f73fb4de0
Asya Smith <asyasmith15@gmail.com>
2022-02-14 22:38:06 -0500

    finished components, just working on imports

---
+++ src/components/taskItem.js
@@ -0,0 +1,16 @@
+import Modal from "./modal"
+import '../stylesheets/taskItem.css'
+
+function TaskItem({onClose, open, title, description}) {
+
+  return (
+    <Modal modalLable='Task Item' onClose={onClose} open={open}>
+      <div className='taskItem'>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    </Modal>
+  )
+}
+
+export default TaskItem
